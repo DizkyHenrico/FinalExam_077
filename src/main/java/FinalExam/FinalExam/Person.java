7d7a39f92f025ecab93167d7ef8cf5da39cf31e6
DizkyHenrico <henricozdzky@gmail.com>
2023-01-13 11:07:10 +0700

    Final Exam

---
+++ src/main/java/FinalExam/FinalExam/Person.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FinalExam.FinalExam;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Dizky Henrico A
+ * Nim : 20200140077
+ */
+@Entity
+@Table(name = "person")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Person.findAll", query = "SELECT p FROM Person p"),
+    @NamedQuery(name = "Person.findById", query = "SELECT p FROM Person p WHERE p.id = :id"),
+    @NamedQuery(name = "Person.findByNik", query = "SELECT p FROM Person p WHERE p.nik = :nik"),
+    @NamedQuery(name = "Person.findByNama", query = "SELECT p FROM Person p WHERE p.nama = :nama"),
+    @NamedQuery(name = "Person.findByTanggalLahir", query = "SELECT p FROM Person p WHERE p.tanggalLahir = :tanggalLahir"),
+    @NamedQuery(name = "Person.findByTimestamp", query = "SELECT p FROM Person p WHERE p.timestamp = :timestamp")})
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "Id")
+    private Integer id;
+    @Column(name = "Nik")
+    private Integer nik;
+    @Column(name = "Nama")
+    private String nama;
+    @Column(name = "TanggalLahir")
+    @Temporal(TemporalType.DATE)
+    private Date tanggalLahir;
+    @Lob
+    @Column(name = "Photo")
+    private byte[] photo;
+    @Basic(optional = false)
+    @Column(name = "Timestamp")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
+
+    public Person() {
+    }
+
+    public Person(Integer id) {
+        this.id = id;
+    }
+
+    public Person(Integer id, Date timestamp) {
+        this.id = id;
+        this.timestamp = timestamp;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNik() {
+        return nik;
+    }
+
+    public void setNik(Integer nik) {
+        this.nik = nik;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Person)) {
+            return false;
+        }
+        Person other = (Person) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "FinalExam.FinalExam.Person[ id=" + id + " ]";
+    }
+    
+}
